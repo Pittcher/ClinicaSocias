4b4334f0f0b0fdc8c497473bcde3de6d04e62e32
Hector Jure <Hector Jure@hjure>
2014-08-16 21:25:06 -0300

    Fix Cripto

--- clinicasocias/clinicasocias-core/src/main/java/com/sistemas/core/commons/CriptHelperImpl.java
+++ clinicasocias/clinicasocias-core/src/main/java/com/sistemas/core/commons/CriptHelperImpl.java
@@ -1,6 +1,7 @@
 package com.sistemas.core.commons;
 
 import java.security.InvalidKeyException;
+import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -30,14 +31,37 @@ public class CriptHelperImpl implements CriptHelper {
 	public String encryt(String texto) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
 		// TODO Auto-generated method stub
 		desCipher.init(Cipher.ENCRYPT_MODE, this.keyKun);
-		return new String(desCipher.doFinal(texto.getBytes()));
+		System.out.println("bytes" + desCipher.doFinal(texto.getBytes()));
+		System.out.println("toestring"+ desCipher.doFinal(texto.getBytes()).toString());
+//		return desCipher.doFinal(texto.getBytes()).toString();
+		return Arrays.toString(desCipher.doFinal(texto.getBytes())).replace("[", "").replace("]","").trim();
+//		return desCipher.doFinal(texto.getBytes()).toString();
 	}
 
 	@Override
 	public String decryt(String texto) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
 		// TODO Auto-generated method stub
 		desCipher.init(Cipher.DECRYPT_MODE, this.keyKun);
-		return new String(desCipher.doFinal(texto));
+//		return new String(desCipher.doFinal(texto.getBytes()));
+//		System.out.println("Decrip_Bytes" + texto.getBytes());
+//		System.out.println("Descrip_toSt" + desCipher.doFinal(texto.getBytes()).toString());
+		
+//		return desCipher.doFinal(texto.getBytes()).toString();
+//		System.out.println("Decrip_Bytes" + texto.toCharArray());
+	    String textoEncriptado = texto.replace("[", "").replace("]","").trim();
+	    
+	    System.out.println("textoEncriptado: " + textoEncriptado);
+	    
+	    byte[] bufferch = new byte[textoEncriptado.split(",").length];
+	    String [] arrsss = textoEncriptado.split(",");
+	    for(int i = 0; i<bufferch.length;i++){
+	    	System.out.println(arrsss[i]);
+	    	bufferch[i]=Integer.valueOf(arrsss[i].trim()).byteValue();
+	    }
+	    
+		return new String(desCipher.doFinal(bufferch));
+		
+//		return desCipher.doFinal(bufferch).toString();
 	}
 
 }
--- clinicasocias/clinicasocias-core/src/test/java/com/test/PruebaTest.java
+++ clinicasocias/clinicasocias-core/src/test/java/com/test/PruebaTest.java
@@ -1,9 +1,11 @@
 package com.test;
 
 
+import java.nio.ByteBuffer;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -12,6 +14,7 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.ShortBufferException;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.apache.commons.codec.DecoderException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,18 +35,108 @@ public class PruebaTest {
 	@Autowired
 	private CriptHelper crip;
 	
+	
+//	@Test
+	public void testTTT() throws DecoderException{
+		String text = "LOLO";
+		System.out.println(text);
+		System.out.println(text.getBytes());
+		
+		System.out.println(new String( text.getBytes()));
+		
+		System.out.println(new String( text.getBytes()).toCharArray() );
+		
+		byte[] btext = text.getBytes();
+		System.out.println("btext:" + btext);
+		
+//		String texto2b = btext.toString();
+//		System.out.println("texto2b: " + texto2b);
+//		System.out.println("texto2b: " + texto2b.getBytes());
+		
+//		System.out.println("test11111: " +texto2b.toCharArray());
+		
+	}
+	
 	@Test
 	public void testUser() throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
 		String texto = "Prueba1";
 		System.out.println(texto);
 		
 		String textoec= crip.encryt(texto);
-		System.out.println(textoec);
+		System.out.println("Test_Cryp"+textoec);
 		
 		String textodesc= crip.decryt(textoec);
-		System.out.println(textodesc);
+		System.out.println("Test_Decr:" + textodesc);
 	}
 
+//	@Test
+	public void testCriptXX() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, ShortBufferException, IllegalBlockSizeException, BadPaddingException{
+		try{
+ 
+		    Cipher desCipher;
+ 
+		    // Create the cipher 
+		    desCipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+ 
+		    // Initialize the cipher for encryption
+		    //K$n1;4nO
+		    SecretKeySpec keyKun = new SecretKeySpec("K$n1;4nO".getBytes(), "DES");
+		    desCipher.init(Cipher.ENCRYPT_MODE, keyKun);
+ 
+		    //sensitive information
+		    byte[] text = "No body can see me".getBytes();
+ 
+		    System.out.println("Text [Byte Format] : " + text);
+		    System.out.println("Text : " + new String(text));
+ 
+		    // Encrypt the text
+		    byte[] textEncrypted = desCipher.doFinal(text);
+ 
+		    System.out.println("Text Encryted : " + textEncrypted);
+ 
+		    // Initialize the same cipher for decryption
+		    desCipher.init(Cipher.DECRYPT_MODE, keyKun);
+ 
+		    // Decrypt the text
+//		    System.out.println("for (byte b : textEncrypted)");
+//		    for (byte b : textEncrypted) {
+//				System.out.println(b);
+//			}
+		    
+		    String textoEncriptado = Arrays.toString(textEncrypted).replace("[", "").replace("]","").trim();
+		    
+		    byte[] bufferch = new byte[textoEncriptado.split(",").length];
+		    String [] arrsss = textoEncriptado.split(",");
+//		    System.out.println("for(int i = 0; i<bufferch.length;i++){");
+		    for(int i = 0; i<bufferch.length;i++){
+//		    	System.out.println(arrsss[i]);
+		    	bufferch[i]=Integer.valueOf(arrsss[i].trim()).byteValue();
+		    }
+		    
+		    System.out.println(" bufferch __ " + bufferch);
+		    System.out.println(" bufferchAAA __ " + Arrays.toString(bufferch) );
+		    
+		    System.out.println("Text PREDecryted : " + Arrays.toString(textEncrypted));
+		    System.out.println( "  bufferch:  " + bufferch);
+
+		    byte[] textDecrypted = desCipher.doFinal(bufferch);	    
+		     
+		    System.out.println("Text Decryted : " + new String(textDecrypted));
+ 
+		}catch(NoSuchAlgorithmException e){
+			e.printStackTrace();
+		}catch(NoSuchPaddingException e){
+			e.printStackTrace();
+		}catch(InvalidKeyException e){
+			e.printStackTrace();
+		}catch(IllegalBlockSizeException e){
+			e.printStackTrace();
+		}catch(BadPaddingException e){
+			e.printStackTrace();
+		} 
+	}
+	
+	
 //	@Test
 	public void testCript() throws NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidKeyException, ShortBufferException, IllegalBlockSizeException, BadPaddingException{
 		try{
@@ -76,8 +169,44 @@ public class PruebaTest {
 		    desCipher.init(Cipher.DECRYPT_MODE, keyKun);
  
 		    // Decrypt the text
-		    byte[] textDecrypted = desCipher.doFinal(textEncrypted);
- 
+//		    byte[] textDecrypted = desCipher.doFinal(textEncrypted);
+		    System.out.println("for (byte b : textEncrypted)");
+		    for (byte b : textEncrypted) {
+				System.out.println(b);
+			}
+//		    System.out.println("char[] arrcah= Arrays.toString(textEncrypted).toCharArray();");
+//		    char[] arrcah= Arrays.toString(textEncrypted).toCharArray();
+//		    for (char c : arrcah) {
+//				System.out.println(c);
+//			}
+		    
+		    
+//		    System.out.println( Integer.valueOf("-114"));
+		    
+		    ByteBuffer bbfr = ByteBuffer.allocate(Arrays.toString(textEncrypted).length());
+		    
+		    
+		    byte[] bufferch = new byte[Arrays.toString(textEncrypted).split(",").length];
+		    String [] arrsss = Arrays.toString(textEncrypted).split(",");
+		    System.out.println("for(int i = 0; i<bufferch.length;i++){");
+		    for(int i = 0; i<bufferch.length;i++){
+		    	System.out.println(arrsss[i]);
+		    	bufferch[i]=Integer.valueOf(arrsss[i].replace("[", "").replace("]","").trim()).byteValue();
+//		    	bbfr.putInt(Integer.valueOf(arrsss[i].replace("[", "").replace("]","").trim()));
+		    }
+		    
+		    System.out.println(" bufferch __ " + bufferch);
+		    System.out.println(" bufferchAAA __ " + Arrays.toString(bufferch) );
+		    
+		    System.out.println(" bbfr -> " + bbfr.array());
+		    System.out.println(" bbfrAA -> " + Arrays.toString(bbfr.array()));
+		    
+		    System.out.println("Text PREDecryted : " + Arrays.toString(textEncrypted));
+		    System.out.println( "  bufferch:  " + bufferch);
+//		    byte[] textDecrypted = desCipher.doFinal(Arrays.toString(textEncrypted).getBytes());
+//		    byte[] textDecrypted = desCipher.doFinal(bbfr.array());
+		    byte[] textDecrypted = desCipher.doFinal(bufferch);	    
+		     
 		    System.out.println("Text Decryted : " + new String(textDecrypted));
  
 		}catch(NoSuchAlgorithmException e){
